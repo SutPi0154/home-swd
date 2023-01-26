aab711ac60e6c63fb49e5af6be58ddf3f1f8a2e8
SutPi0154 <sutpi070@gmail.com>
2023-01-26 10:24:43 +0630

    list-builder finished

--- 3-learn-js/DOM-BOM-JS/js/note.js
+++ 3-learn-js/DOM-BOM-JS/js/note.js
@@ -1,13 +1,17 @@
-const creatBtn = document.querySelector("#createBtn");
+const createBtn = document.querySelector("#createBtn");
 const lists = document.querySelector("#lists");
 const textInput = document.querySelector("#textInput");
+const total = document.querySelector("#total");
+const doneTotal = document.querySelector("#doneTotal");
+
+const data = ["banana", "apple", "orange", "mango"];
 
 const createLi = (text) => {
-  const dyID = "flexCheck" + Date.now();
+  const dyID = "flexCheck" + Math.random();
   const li = document.createElement("li");
   li.classList = "list-group-item d-flex align-items-center";
   // li.ondblclick = edit(event);
-  li.addEventListener("dblclick", edit);
+  // li.addEventListener("dblclick", edit);
 
   li.innerHTML = `    
 <div class="input-group">
@@ -24,10 +28,24 @@ const createLi = (text) => {
     </label>
   </div>
   </div>
-  <button class="btn btn-sm btn-danger ms-auto rounded " onclick="del(event)">
-    Del
-  </button> `;
+  <div class=" btn-group">
+  <button class="btn btn-sm btn-outline-dark edit">
+    <i class="bi bi-pencil pe-none "></i>
+  </button> 
+  <button class="btn btn-sm btn-outline-dark del">
+  <i class="bi bi-trash3 pe-none "></i>
+</button> 
+  </div>`;
   return li;
+};
+
+const counter = () => {
+  const totalCount = lists.children.length;
+  const doneTotalCount = [...lists.children].filter(
+    (el) => el.querySelector(".form-check-input").checked === true
+  ).length;
+  total.innerText = totalCount;
+  doneTotal.innerText = doneTotalCount;
 };
 
 // add lists button function
@@ -37,11 +55,14 @@ const addList = () => {
     lists.append(createLi(textInput.value));
     textInput.value = null;
   } else alert("Input text is empty");
+  counter();
 };
+
+// const edit = () => {};
 
 // lists.append(createLi(`apple`));
 
-creatBtn.addEventListener(`click`, addList);
+createBtn.addEventListener(`click`, addList);
 
 textInput.addEventListener("keyup", (event) => {
   if (event.keyCode === 13) {
@@ -51,20 +72,39 @@ textInput.addEventListener("keyup", (event) => {
 
 const del = (event) => {
   if (confirm("Are you sure to delete?")) {
-    event.target.parentElement.remove();
+    event.target.closest(".list-group-item").remove();
   }
+  counter();
 };
 
 const done = (event) => {
   event.target.nextElementSibling.classList.toggle(
     `text-decoration-line-through`
   );
+  counter();
 };
+data.forEach((d) => lists.append(createLi(d)));
 
 const edit = (event) => {
   // console.log(event.target);
-  const old = event.target.querySelector(".form-check-label");
+  const old = event.target.closest("li").querySelector(".form-check-label");
   const newText = prompt("Input new Text", old.innerText);
-  // console.log(newText);
-  old.innerText = newText;
+  if (newText && newText.trim()) {
+    old.innerText = newText;
+  }
+  counter();
 };
+
+// [...lists.children].forEach((li) => {
+//   li.querySelector(".edit").addEventListener("click", edit);
+//   li.querySelector(".del").addEventListener("click", del);
+// });
+
+lists.addEventListener("click", (event) => {
+  if (event.target.classList.contains("del")) {
+    del(event);
+  } else if (event.target.classList.contains("edit")) {
+    edit(event);
+  }
+});
+window.addEventListener("load", counter());
